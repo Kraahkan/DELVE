67c5f590490f24dfc8e7027cee1b62f62f51f7bc
Josh <kraahkan@gmail.com>
2020-02-04 14:23:17 -0800

    Start of soundcontroller

--- app/src/main/java/dependency/greendao/test/tinder/directional/SoundController.java
+++ app/src/main/java/dependency/greendao/test/tinder/directional/SoundController.java
@@ -2,6 +2,7 @@ package dependency.greendao.test.tinder.directional;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Debug;
 import android.util.Log;
 
@@ -9,13 +10,17 @@ import java.util.ArrayList;
 
 public class SoundController {
 
+    String oldSound = "";
+    Context activityContext;
+
     public void initialize(Context context) {
 
+        activityContext = context;
         parseResources(context);
 
     }
 
-    private void parseResources(Context context) {
+    private void parseResources(Context context) { // need to rewrite this
 
         Resources resources = context.getResources();
 
@@ -23,25 +28,45 @@ public class SoundController {
 
         ArrayList<Integer> id = new ArrayList<Integer>();
         for (int i = 0; i <= 79; i++) {
-
         }
 
         id.add(context.getResources().getIdentifier("fx_chest", "raw", context.getPackageName())); // don't include file type
         Log.d("resources",id.get(0).toString());
 
+   }
+
+   private void playOneShot(String soundName, int startTime, int duration) {
 
    }
 
-   private void fadeIn() {
-
+   public void playMusic(String musicName) { // only one music at a time
+       crossFade(musicName);
    }
 
-   private void fadeOut() {
+    public void playAmbiance(String ambianceName) { // only one ambiance at a time
+        crossFade(ambianceName);
+    }
 
+
+   private void crossFade(String newSound) {
+        fadeIn(newSound);
+        fadeOut(oldSound);
+
+        oldSound = newSound;
    }
 
-   public void crossFade() {
-        fadeIn();
-        fadeOut();
-   }
+    private void fadeIn(String soundName) {
+
+    }
+
+    private void fadeOut(String soundName) { // fade out
+
+    }
+
+    private void playSound(String soundName) { // WORK ON THIS
+       // Uri.
+        //playr = MediaPlayer.create(activityContext,R.raw. + soundName);
+       // MediaPlayer.cre
+    }
+
 }
