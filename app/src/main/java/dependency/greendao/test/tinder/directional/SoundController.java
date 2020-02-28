4176815afbb5dff0e1ee55751030cc793996ff70
Josh <kraahkan@gmail.com>
2020-02-27 19:53:34 -0800

    Initial merge, seems to be a success
    done manually

---
+++ app/src/main/java/dependency/greendao/test/tinder/directional/SoundController.java
@@ -0,0 +1,47 @@
+package dependency.greendao.test.tinder.directional;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class SoundController {
+
+    public void initialize(Context context) {
+
+        parseResources(context);
+
+    }
+
+    private void parseResources(Context context) {
+
+        Resources resources = context.getResources();
+
+        //resources.obtainTypedArray(R.raw);
+
+        ArrayList<Integer> id = new ArrayList<Integer>();
+        for (int i = 0; i <= 79; i++) {
+
+        }
+
+        id.add(context.getResources().getIdentifier("fx_chest", "raw", context.getPackageName())); // don't include file type
+        Log.d("resources", id.get(0).toString());
+
+
+    }
+
+    private void fadeIn() {
+
+    }
+
+    private void fadeOut() {
+
+    }
+
+    public void crossFade() {
+        fadeIn();
+        fadeOut();
+    }
+}
